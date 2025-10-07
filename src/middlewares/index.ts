e05cc2c891d59c14275b93ec2b20b4040dc9857d
Dmitry Sternyaev <17826630+usebooz@users.noreply.github.com>
2025-10-08 00:29:02 +0300

    fix: all chats

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,7 +1,7 @@
 import { Bot } from 'grammy';
 import { logger } from '../utils/logger.js';
 import { loggingMiddleware } from './logging.middleware.js';
-import { chatFilterMiddleware } from './chat.middleware.js';
+// import { chatFilterMiddleware } from './chat.middleware.js';
 import { sessionInitMiddleware } from './session.middleware.js';
 import type { MyContext } from '../types/index.js';
 import { rplReadMiddleware } from './rpl.middleware.js';
@@ -9,7 +9,7 @@ import { rplReadMiddleware } from './rpl.middleware.js';
 export function setupMiddlewares(bot: Bot<MyContext>): void {
   // Setup bot-specific middlewares
   bot.use(loggingMiddleware);
-  bot.use(chatFilterMiddleware);
+  // bot.use(chatFilterMiddleware);
   bot.use(sessionInitMiddleware);
   bot.use(rplReadMiddleware);
 
